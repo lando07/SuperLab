6882f4c084fe87ea63fc4d6bbfa5541599ade758
lando07 <96402540+lando07@users.noreply.github.com>
2023-09-22 13:31:37 -0500

    Buttons?

--- src/SuperLab/Controller.java
+++ src/SuperLab/Controller.java
@@ -1,16 +1,22 @@
 package SuperLab;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
 
-    @FXML
-    private Label label;
+    public void up(ActionEvent e){
+        System.out.println("UP");
+    }
 
-    public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+    public void down(ActionEvent e){
+        System.out.println("down");
+    }
+    public void left(ActionEvent e){
+        System.out.println("left");
+    }
+    public void right(ActionEvent e){
+        System.out.println("right");
     }
 }
